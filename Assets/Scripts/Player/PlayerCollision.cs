8173b19b8230dec3c534129cb1c66586929e968f
Lilou18 <60436875+Lilou18@users.noreply.github.com>
2024-11-01 12:50:28 -0400

    fix portal end and add comments

--- Assets/Scripts/Player/PlayerCollision.cs
+++ Assets/Scripts/Player/PlayerCollision.cs
@@ -7,15 +7,18 @@ using UnityEngine.SceneManagement;
 public class PlayerCollision : MonoBehaviour
 {
     private PlayerHealth playerHealth;
+    Animator animator;
 
     public static event Action OnGameEnd;
 
     private void Start()
     {
         playerHealth = GetComponent<PlayerHealth>();
+        animator = GetComponentInChildren<Animator>();
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // If the player falls then we make him repsawn and loose a life
         if(collision.gameObject.tag == "Fall")
         {
             playerHealth.TakeDamage(1);
@@ -23,23 +26,30 @@ public class PlayerCollision : MonoBehaviour
             // Keep the camera from following the player when he falls into a hole
             Camera.main.gameObject.GetComponent<CameraMovement>().enabled = false;
         }
-        else if(collision.gameObject.tag == "FireBall")
+        else if(collision.gameObject.tag == "FireBall") // If the player enter in collision with a fireball then he takes damage
         {
             playerHealth.TakeDamage(1);
             Destroy(collision.gameObject);
             //SoundManager.Instance.PlaySound(SoundManager.Instance.Test);
         }
-        else if(collision.gameObject.tag == "CrushingBlock")
+        else if(collision.gameObject.tag == "CrushingBlock") // If the player touch the crushing block he loose a life
         {
             playerHealth.TakeDamage(1);
         }
-        else if(collision.gameObject.tag == "End")
+        else if(collision.gameObject.tag == "End") // The player arrived at the portal which is the ending
         {
-            OnGameEnd.Invoke();
-        }
-        
+            animator.SetBool("IsTeleporting", true);
+            StartCoroutine("WaitTeleportingAnimation");
+        }        
+    }
+    // We wait until the portal animation si finished before showing the score panel
+    private IEnumerator WaitTeleportingAnimation()
+    {
+        yield return new WaitForSeconds(1f);
+        OnGameEnd.Invoke();
     }
 
+    // If the player enter in collision with an enemy then he takes damage
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Enemy")
