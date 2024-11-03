942b824ec92b8c89fd9d2522540bed87db4c2111
Lilou18 <60436875+Lilou18@users.noreply.github.com>
2024-11-03 01:17:10 -0400

    friend and friend manager comment

--- Assets/Scripts/Friend.cs
+++ Assets/Scripts/Friend.cs
@@ -5,72 +5,51 @@ using static UnityEngine.GraphicsBuffer;
 
 public class Friend : MonoBehaviour
 { 
-    [SerializeField] private Transform followed;
+    // This class manage the behaviour of the friends
+
+    [SerializeField] private Transform followed; // Transform that the friend must follow
     [SerializeField] public Transform Followed { get{return followed;} set{ if (value != null) { followed = value; }} }
     [SerializeField] float speed;
-    [SerializeField] GameObject friendMarker;
+    [SerializeField] GameObject friendMarker; // This his the GameObject attached to the friend, that the new friend will follow
     public GameObject FriendMarker { get { return friendMarker; } }
-    private bool markerRight; // Is the marker is on the right side of the friend
+    private bool markerRight; // Is the marker on the right side of the friend
 
     private Animator animator;
-    private Vector3 currentVelocity = Vector3.zero; // Vitesse pour SmoothDamp
 
     private void Start()
     {
         markerRight = true;
         animator = GetComponentInChildren<Animator>();
     }
-    //private void Update()
-    //{
-
-    //    if (followed != null)
-    //    {
-    //        float horizontalInput = Input.GetAxis("Horizontal");
-
-    //        if (horizontalInput > 0 && markerRight) // The friend is mooving to the right and the marker is on the right side
-    //        {
-    //            FlipMarkerFriend();
-    //        }
-    //        else if (horizontalInput < 0 && !markerRight) //// The friend is mooving to the left and the marker is on the left side
-    //        {
-    //            FlipMarkerFriend();
-    //        }
-
-    //        if (Vector3.Distance(transform.position, followed.position) > 0.1f) // L'ami est encore en mouvement
-    //        {
-    //            animator.SetBool("IsWalkingRight", transform.position.x < followed.position.x);
-    //            animator.SetBool("IsWalkingLeft", transform.position.x > followed.position.x);
-    //        }
-    //        else // L'ami est arrivé à la position cible
-    //        {
-    //            animator.SetBool("IsWalkingLeft", false);
-    //            animator.SetBool("IsWalkingRight", false);
-    //        }
-    //        transform.position = Vector3.Lerp(transform.position, followed.position, Time.deltaTime * speed);
-    //    }
-    //}
-
+   
     private void FixedUpdate()
     {
         if (followed != null)
         {
             float horizontalInput = Input.GetAxis("Horizontal");
 
-            if (horizontalInput > 0 && markerRight) // The friend is mooving to the right and the marker is on the right side
+            // If the player is mooving to the right and the marker is on the right side
+            // We change the position of the friend marker to be on the left so the friends that follow
+            // will look like they are following him
+            if (horizontalInput > 0 && markerRight)
             {
                 FlipMarkerFriend();
             }
-            else if (horizontalInput < 0 && !markerRight) //// The friend is mooving to the left and the marker is on the left side
+            // If the player is mooving to the left and the marker is on the left side
+            // We change the position of the friend marker to be on the right so the friends that follow
+            // will look like they are following him
+            else if (horizontalInput < 0 && !markerRight)
             {
                 FlipMarkerFriend();
             }
 
-            if (Vector3.Distance(transform.position, followed.position) > 0.1f) // L'ami est encore en mouvement
+            // If the friend is still moving to join the player or the other friends then we continue his walking animation
+            if (Vector3.Distance(transform.position, followed.position) > 0.1f)
             {
                 animator.SetBool("IsWalkingRight", transform.position.x < followed.position.x);
                 animator.SetBool("IsWalkingLeft", transform.position.x > followed.position.x);
             }
-            else // L'ami est arrivé à la position cible
+            else // The friend has joined the player or the other friends
             {
                 animator.SetBool("IsWalkingLeft", false);
                 animator.SetBool("IsWalkingRight", false);
--- Assets/Scripts/FriendManager.cs
+++ Assets/Scripts/FriendManager.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class FriendManager : MonoBehaviour
 {
-    [SerializeField] private List<Friend> friends;
-    [SerializeField] Transform friendMarker;
+    // Manage the collection of friends the player can find and have them follow in the the game
+
+    [SerializeField] private List<Friend> friends; // List that keep track of all the friends found
+    [SerializeField] Transform friendMarker; // Position that must follow the new found friend (only the first one)
     [SerializeField] public int NumberFriends { get; private set; }
 
     private void Start()
@@ -15,23 +17,26 @@ public class FriendManager : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // Check if the player trigger a collision with a friend
         if(collision.gameObject.tag == "Friend")
         {
             SoundManager.Instance.PlaySound("Friend");
             Friend newFriend = collision.gameObject.GetComponent<Friend>();
-            // On ne veut plus que le joueur entre en collision avec celui-ci
+
+            // We don't want the player to trigger a collision again with him
             newFriend.gameObject.GetComponent<CircleCollider2D>().enabled = false; 
+
             if(friends.Count == 0)
             {
-                newFriend.Followed = friendMarker; // On indique au premier ami trouvé qu'il doit suivre le joueur
+                newFriend.Followed = friendMarker; // Set the first friend to follow the player
             }
             else
             {
-                // Les autres amis suivent le dernier ami trouvé
+                // The other friends must follow the last friend found
                 newFriend.Followed = friends[friends.Count - 1].FriendMarker.transform;
             }
             friends.Add(newFriend);
-            NumberFriends++;
+            NumberFriends++; // Update the number of friends found
         }
     }
 }
