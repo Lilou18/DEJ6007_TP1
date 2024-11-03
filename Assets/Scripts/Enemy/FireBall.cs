2004cb53a14d9a59f9dae5486c25583fabdbbc0a
Lilou18 <60436875+Lilou18@users.noreply.github.com>
2024-11-02 23:47:42 -0400

    fireball comments

--- Assets/Scripts/Enemy/FireBall.cs
+++ Assets/Scripts/Enemy/FireBall.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class FireBall : MonoBehaviour
 {
+    // Manage the collision for the Fireballs created by the enemies and the player
+
     // Destroy when enter in contact with anything
     private void OnCollisionEnter2D(Collision2D collision)
     {
