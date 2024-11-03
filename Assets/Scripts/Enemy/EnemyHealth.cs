8bc0312a7ddc6f4de8e8d360c18a8e0fafb627b9
Lilou18 <60436875+Lilou18@users.noreply.github.com>
2024-11-02 23:15:02 -0400

    comment enemy health

--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
+    // This class manage the enemies health
+
     [SerializeField] private int health;
     public int Health { get { return health; } }
 
+    // Reduce the health of the enemy and destroy it when he dies
     public void TakeDamage(int damage)
     {
         health -= damage;
