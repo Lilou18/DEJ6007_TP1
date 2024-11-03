720c8713811b52c008544781061c80bd888ecef7
Lilou18 <60436875+Lilou18@users.noreply.github.com>
2024-11-03 00:58:11 -0400

    game manager comment

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,9 +6,10 @@ using System;
 
 public class GameManager : MonoBehaviour
 {
+    // This class control high-level game events
 
-    [SerializeField] GameObject deadPanel;
-    [SerializeField] GameObject scorePanel;
+    [SerializeField] GameObject deadPanel;  // Panel to display when the player dies
+    [SerializeField] GameObject scorePanel; // Panel to display when the player finish the game
 
     private void Start()
     {
@@ -16,17 +17,21 @@ public class GameManager : MonoBehaviour
         PlayerCollision.OnGameEnd += FinishLevel;
     }    
 
+    // Display the panel that propose to the player to play again
     public void PlayerDie()
     {
         deadPanel.SetActive(true);
-        Time.timeScale = 0f;
+        Time.timeScale = 0f;    // Pause the game when the player dies
     }
+
+    // Restard the level and unpause the game
     public void RestartLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         Time.timeScale = 1f;
     }
 
+    // Display the score panel when the player wins
     public void FinishLevel()
     {
         scorePanel.SetActive(true);
