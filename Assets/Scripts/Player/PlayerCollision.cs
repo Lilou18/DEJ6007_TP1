dffb3a6b699a2ac0ebdbce8d9c040e87543303d1
Lilou18 <60436875+Lilou18@users.noreply.github.com>
2025-05-24 18:44:13 -0400

    fix end animation

--- Assets/Scripts/Player/PlayerCollision.cs
+++ Assets/Scripts/Player/PlayerCollision.cs
@@ -9,6 +9,7 @@ public class PlayerCollision : MonoBehaviour
     // This class manage many collisions with the player
 
     private PlayerHealth playerHealth;
+    private PlayerMovement playerMovement;
     Animator animator;
 
     public static event Action OnGameEnd; // Invoke the event when the game end
@@ -17,6 +18,7 @@ public class PlayerCollision : MonoBehaviour
     private void Start()
     {
         playerHealth = GetComponent<PlayerHealth>();
+        playerMovement = GetComponent<PlayerMovement>();
         animator = GetComponentInChildren<Animator>();        
     }
     private void OnTriggerEnter2D(Collider2D collision)
@@ -34,12 +36,28 @@ public class PlayerCollision : MonoBehaviour
             playerHealth.TakeDamage(1);
         }
         else if(collision.gameObject.tag == "End") // The player arrived at the portal which is the ending
-        {
+        {          
+            playerMovement.rb.bodyType = RigidbodyType2D.Kinematic;
+            playerMovement.rb.velocity = Vector3.zero;
+            playerMovement.enabled = false;
             SoundManager.Instance.PlaySound("Ending");
             animator.SetBool("IsTeleporting", true);
+            StartCoroutine(EndingAnimation(collision.transform));
             StartCoroutine("WaitTeleportingAnimation");
         }        
     }
+
+    private IEnumerator EndingAnimation(Transform portalTransform)
+    {
+        float moveSpeed = 2f;
+        while(Vector3.Distance(this.transform.position, portalTransform.position) > 0.1f)
+        {
+            this.transform.position = Vector3.MoveTowards(this.transform.position, portalTransform.position, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        
+    }
+
     // We wait until the portal animation si finished before showing the score panel
     private IEnumerator WaitTeleportingAnimation()
     {
